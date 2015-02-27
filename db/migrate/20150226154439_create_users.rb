ae3045151ddfff0a9e1d5c7531dbb7ae51a50977
Kristen MacFarlane <krismacfarlane@gmail.com>
2015-02-27 09:56:16 -0500

    removes faulty migration file

--- db/migrate/20150226154439_create_users.rb
+++ db/migrate/20150226154439_create_users.rb
@@ -1,9 +1,9 @@
-class CreateUsers < ActiveRecord::Migration
-  def change
-    create_table :users do |t|
-      t.string :
-      t.
-      t.timestamps null: false
-    end
-  end
-end
+# class CreateUsers < ActiveRecord::Migration
+#   def change
+#     create_table :users do |t|
+#       t.string :
+#       t.
+#       t.timestamps null: false
+#     end
+#   end
+# end
